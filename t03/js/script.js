5933cf7e2d4d8b81c399e12a7f6b65490e2d2097
Sergij Skuleba <sskuleba@e1r5p3.unit.ua>
2020-09-20 21:57:40 +0300

    3task

---
+++ t03/js/script.js
@@ -0,0 +1,16 @@
+function myFunc() {
+  alert('Freedom is power');
+}
+
+myFunc();
+
+function countMySquare(number) {
+  return number * number;
+}
+
+alert('The square of 2 = ' + countMySquare(2));
+
+
+let age = prompt('Сколько тебе лет?', 1);
+
+alert('The square of ' + age + ' = ' + countMySquare(age)); // Тебе 100 лет!
