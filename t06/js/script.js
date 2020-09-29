f56f511a1964531a4fecd12c0e2408e2e5c3cf8e
ssqool <skyleba@ukr.net>
2020-09-29 13:51:15 +0300

    task6

---
+++ t06/js/script.js
@@ -0,0 +1,20 @@
+var number = 12;
+const BigInt = 1234567890123456789012345678901234567890n;
+var String = 'sdadcf';
+var boolean = true;
+let age = null;
+let something;
+let user = new Object();
+var sym = Symbol(52);
+var a = function funct(n) {};
+
+alert(`${typeof(number)} 
+${typeof(BigInt)} 
+${typeof(String)} 
+${typeof(boolean)} 
+${typeof(age)} 
+${typeof(something)}
+${typeof(user)}
+${typeof(sym)}
+${typeof(a)}`);
+
