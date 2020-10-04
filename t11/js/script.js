2a656fdaa380dc89ded7c3cd63eb989151279ad2
ssqool <skyleba@ukr.net>
2020-10-04 18:02:27 +0300

    t11

---
+++ t11/js/script.js
@@ -0,0 +1,28 @@
+// // the first loop
+// var i = 6;
+// var res1= ' ';
+//
+// do {
+//   res1+= i + ' ';
+//   i++;
+// } while(i < 5);
+//
+// // the second loop
+// i = 6;
+// var res2 = ' ';
+// while(i < 5) {
+//   alert(res2);
+//   res2+= i + ' ';
+//   i++;
+// }
+//
+// alert('do while: '+ res1 + 'while: ' + res2);
+
+var num = 2;
+var i = 1;
+var ret;
+
+while (i <= 10) {
+  console.log(num*i);
+  i++;
+}
