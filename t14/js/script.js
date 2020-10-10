969a35503e5f8a6aedb7119013d47c4ab9fe857e
ssqool <skyleba@ukr.net>
2020-10-10 14:55:25 +0300

    task14

---
+++ t14/js/script.js
@@ -0,0 +1,8 @@
+function total(addCount, addPrice, currentTotal=0) {
+  return currentTotal += addCount * addPrice;
+}
+
+let sum = total(1, 0.1);
+sum = total(1, 0.2, sum);
+sum= total(1, 0.78, sum);
+console.log(sum); // will returns 1.08
